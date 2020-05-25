c6725f18b3ef27593e97e6cdaee992e7fb005cb0
yanghang <470193837@qq.com>
2020-05-25 12:55:07 +0800

    扩展方法

--- src/utils/util.js
+++ src/utils/util.js
@@ -908,3 +908,107 @@ export const checkPwd = (str) => {
     }
     return Lv;
 }
+
+/**
+ * 函数节流器
+ * @param  {Function} fn 需要执行性的函数
+ * @param  {number} time 时间戳
+ * @param  {number} interval 间隔时间
+ */
+export const debouncer = (fn, time, interval = 200) => {
+    if (time - (window.debounceTimestamp || 0) > interval) {
+        fn && fn();
+        window.debounceTimestamp = time;
+    }
+}
+
+/**
+ * 在字符串中插入新字符串
+ * @param {string} soure 源字符
+ * @param {string} index 插入字符的位置
+ * @param {string} newStr 需要插入的字符
+ * @returns {string} 返回新生成的字符
+ */
+export const insertStr = (soure, index, newStr) => {
+    var str = soure.slice(0, index) + newStr + soure.slice(index);
+    return str;
+}
+
+/**
+* 判断两个对象是否键值相同
+* @param  {Object}  a 第一个对象
+* @param  {Object}  b 第一个对象
+* @return {Boolean}   相同返回true，否则返回false
+*/
+export const isObjectEqual = (a, b) => {
+    var aProps = Object.getOwnPropertyNames(a);
+    var bProps = Object.getOwnPropertyNames(b);
+
+    if (aProps.length !== bProps.length) {
+        return false;
+    }
+
+    for (var i = 0; i < aProps.length; i++) {
+        var propName = aProps[i];
+
+        if (a[propName] !== b[propName]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/**
+ * 16进制颜色转RGB\RGBA字符串
+ * @param  {String} val 16进制颜色值
+ * @param  {Number} opa 不透明度，取值0~1
+ * @return {String}     转换后的RGB或RGBA颜色值
+ */
+export const colorToRGB = (val, opa) => {
+
+    var pattern = /^(#?)[a-fA-F0-9]{6}$/; //16进制颜色值校验规则
+    var isOpa = typeof opa == 'number'; //判断是否有设置不透明度
+
+    if (!pattern.test(val)) { //如果值不符合规则返回空字符
+        return '';
+    }
+
+    var v = val.replace(/#/, ''); //如果有#号先去除#号
+    var rgbArr = [];
+    var rgbStr = '';
+
+    for (var i = 0; i < 3; i++) {
+        var item = v.substring(i * 2, i * 2 + 2);
+        var num = parseInt(item, 16);
+        rgbArr.push(num);
+    }
+
+    rgbStr = rgbArr.join();
+    rgbStr = 'rgb' + (isOpa ? 'a' : '') + '(' + rgbStr + (isOpa ? ',' + opa : '') + ')';
+    return rgbStr;
+}
+
+/**
+   * 追加url参数
+   * @param {string} url url参数
+   * @param {string|object} key 名字或者对象
+   * @param {string} value 值
+   * @return {string} 返回新的url
+   * @example
+   * appendQuery('lechebang.com', 'id', 3);
+   * appendQuery('lechebang.com?key=value', { cityId: 2, cityName: '北京'});
+   */
+export const appendQuery = (url, key, value) => {
+    var options = key;
+    if (typeof options == 'string') {
+        options = {};
+        options[key] = value;
+    }
+    options = $.param(options);
+    if (url.includes('?')) {
+        url += '&' + options
+    } else {
+        url += '?' + options
+    }
+    return url;
+}
