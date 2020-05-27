00dabd7085f0ddb00da1e8e9d78b555a8b0c88e6
yanghang <470193837@qq.com>
2020-05-27 17:14:16 +0800

    添加 数组分片

--- src/utils/util.js
+++ src/utils/util.js
@@ -1022,3 +1022,18 @@ export const getArrRepeat = (arr1,arr2) =>{
         return arr2.includes(item)
     })
 } 
+
+
+
+/**
+ * 将数组分片
+ * 列子[1,2,3,4,5,6,7,8] [[1,2,3],[4,5,6],[7,8]]
+ */
+utils.arrChunk = (data=[],space=5) => {
+    var result = [];
+    for (var i = 0, len = data.length; i < len; i += space) {
+        result.push(data.slice(i, i + space));
+    }
+    return {data:result,total:data.length,space};
+}
+
