91d2d54a193bd3d3dd7982df0d45fc58d3bc05fc
yanghang <470193837@qq.com>
2020-06-01 15:13:57 +0800

    修复一个导出问题

--- src/utils/util.js
+++ src/utils/util.js
@@ -1029,7 +1029,7 @@ export const getArrRepeat = (arr1,arr2) =>{
  * 将数组分片
  * 列子[1,2,3,4,5,6,7,8] [[1,2,3],[4,5,6],[7,8]]
  */
-utils.arrChunk = (data=[],space=5) => {
+export const arrChunk = (data=[],space=5) => {
     var result = [];
     for (var i = 0, len = data.length; i < len; i += space) {
         result.push(data.slice(i, i + space));
